201b84facf91c9c2f5806e371c291dbcf3c194d3
pragatidixit <dixitpragati272@gmail.com>
2026-06-30 08:33:08 +0530

    sync async loop example

---
+++ async.js
@@ -0,0 +1,21 @@
+console.log("hii");
+console.log("sync");
+setTimeout(()=>
+{
+    console.log("after 2 sec");
+},2000);
+
+console.log("10");
+setTimeout(()=>
+{
+    console.log("after 4 sec");
+},4000);
+setTimeout(()=>
+{
+    console.log("after 6 sec");
+},6000);
+
+console.log("when");
+console.log("why");
+console.log("how");
+
